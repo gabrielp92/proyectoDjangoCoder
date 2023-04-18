25695c96d9d37dcfa5b13b1e1510ad6def3adc36
Gabriel Paez <gpaez1992@gmail.com>
2023-04-18 15:09:16 -0300

    configuración de admin.py

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -5,10 +5,16 @@ class Curso(models.Model):
     nombre = models.CharField(max_length=50)
     comision = models.IntegerField()
     
+    def __str__(self):
+        return f'{self.nombre}-{self.comision}'
+    
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
+    
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
     
 class Profesor(models.Model):
     nombre = models.CharField(max_length=50)
@@ -16,7 +22,13 @@ class Profesor(models.Model):
     email = models.EmailField(max_length=254)
     profesion = models.CharField(max_length=50)
     
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
+    
 class Entregable(models.Model):
     nombre = models.CharField(max_length=50)
     fecha_entrega = models.DateField()
     entregado = models.BooleanField()
+    
+    def __str__(self):
+        return f'{self.nombre}'
