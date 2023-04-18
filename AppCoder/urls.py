f06d9226d07987ede2aea1c3ecc0b62c4f4caf15
Gabriel Paez <gpaez1992@gmail.com>
2023-04-18 01:12:15 -0300

    herencia y navegación entre templates

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -3,9 +3,9 @@ from AppCoder import views
 
 urlpatterns = [
     path('insertacurso/<nombre>/<comision>', views.curso),
-    path('listacursos/', views.lista_cursos),
-    path('', views.inicio),
-    path('estudiantes/', views.estudiantes),
-    path('profesores/', views.profesores),
-    path('entregables', views.entregables)
+    path('listacursos/', views.lista_cursos, name="Cursos"),
+    path('', views.inicio, name="Inicio"),
+    path('estudiantes/', views.estudiantes, name="Estudiantes"),
+    path('profesores/', views.profesores, name="Profesores"),
+    path('entregables', views.entregables, name="Entregables")
 ]
--- ProyectoCoder/urls.py
+++ ProyectoCoder/urls.py
@@ -15,15 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-#from AppCoder.views import saludo, segunda_view, dia_de_hoy, saluda_con_nombre, probando_template
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app-coder/', include('AppCoder.urls'))
-    
-    #path('saludo/', saludo),
-    #path('otravista/', segunda_view),
-    #path('diadehoy/', dia_de_hoy),
-    #path('saluda-nombre/<nombre>', saluda_con_nombre),
-    #path('probando-template/', probando_template)
+
 ]
